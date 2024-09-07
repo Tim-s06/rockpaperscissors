42f3cadb171817b75b6937cf71a4335376164bd7
Tim-s06 <timsandro.nguyen@gmail.com>
2024-09-07 21:43:47 +0200

    Added the logic to play a single round deciding the winner and adding to the score of the winner

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-
+let humanScore = 0;
+let computerScore = 0;
+function playRound() {
+    const computerSelection = getComputerChoice();
+    console.log("Computer Choice: " + computerSelection);
+    const humanSelection = getHumanChoice().toLowerCase();
+    
+    
+    if(humanSelection != "rock" && humanSelection !="paper" && humanSelection!="scissors") {
+        console.log("You lose! Wrong input!")
+        computerScore++;
+    } else if(humanSelection == computerSelection.toLowerCase()) {
+        console.log("Draw! " + computerSelection +  " is the same as " + computerSelection + "!");
+    }else if(humanSelection == "rock" && computerSelection =="Paper") {
+        console.log("You lose! Paper coats Rock!");
+        computerScore++;
+    } else if(humanSelection == "rock" && computerSelection =="Scissors") {
+        console.log("You win! Rock beats Scissors!");
+        humanScore++;
+    } else if(humanSelection == "paper" && computerSelection=="Rock") {
+        console.log("You win! Paper coats Rock!");
+        humanScore++;
+    }else if(humanSelection == "paper" && computerSelection=="Scissors") {
+        console.log("You lose! Scissors cut Paper!");
+        computerScore++;
+    }else if(humanSelection == "scissors" && computerSelection=="Rock") {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore++;
+    }
+    else if(humanSelection == "scissors" && computerSelection=="Paper") {
+        console.log("You win! Scissors cut Paper!");
+        humanScore++;
+    }
+}
 function getComputerChoice() {
     let number = Math.floor(Math.random()*100);
     let choice = "";
